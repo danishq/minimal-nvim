442c0a37d41bcaf54916670c1dec30e30f1b8973
Danish Qureshi <dqureshi08@gmail.com>
2025-10-06 19:04:59 +0530

    added plugin lualine

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,8 @@
 return {
-	"nvim-treesitter/nvim-treesitter"
+	"nvim-treesitter/nvim-treesitter",
+    build=":TSUpdate",
+    config = function()
+        require("config.nvimtreesitter").setup()
+    end
+
 }
